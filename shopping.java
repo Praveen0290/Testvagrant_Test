05031c69e43a365f40b36f0754fa932b3a6e245d
Praveen0290 <139365261+Praveen0290@users.noreply.github.com>
2023-11-18 13:15:19 +0530

    Add files via upload

---
+++ shopping.java
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int a=1100;
+int b=900;
+int c=200;
+int d=100;
+public static void main(String[] args)
+{
+    if(a)
+    {
+      a%18;
+    }
+    if(b)
+    {
+      b%12;
+    }
+    if(c)
+    {
+      c%28;
+    }
+    if(d)
+    {
+      d%0;
+    }
+}
